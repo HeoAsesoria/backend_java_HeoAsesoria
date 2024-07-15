fb5e07398513d469e022134ff8da5c866c3b71db
jicasa03 <jimmy.carbajal.camposol@viaexperis.pe>
2024-07-15 00:23:47 -0500

    surcursla

--- src/main/java/com/pe/HeoComisiones/DTOs/admin/AdminContratosDTO.java
+++ src/main/java/com/pe/HeoComisiones/DTOs/admin/AdminContratosDTO.java
@@ -3,15 +3,16 @@ package com.pe.HeoComisiones.DTOs.admin;
 import java.util.Date;
 
 public record AdminContratosDTO(
-        Integer idcontrato,
-        String codigoContrato,
-        String urlContrato,
-        Date fechaCreacion,
-        String tipoContrato,
-        String nombre_cliente,
-        String dni_cliente,
-        String tipo_documento_cliente,
-        String tipo_moneda
+                Integer idcontrato,
+                String codigoContrato,
+                String urlContrato,
+                Date fechaCreacion,
+                String tipoContrato,
+                String nombre_cliente,
+                String dni_cliente,
+                String tipo_documento_cliente,
+                String tipo_moneda,
+                Integer id_sucursal
 
 ) {
 }
