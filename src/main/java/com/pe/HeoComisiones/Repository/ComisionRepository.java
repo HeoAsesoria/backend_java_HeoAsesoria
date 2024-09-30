c5627eb80fb6f8ce8c87fc842164ee03adedf601
DESKTOP-77HES0B\User <jimmycarbajalsanchez@gmail.com>
2024-09-30 10:02:04 -0500

    jimmy

--- src/main/java/com/pe/HeoComisiones/Repository/ComisionRepository.java
+++ src/main/java/com/pe/HeoComisiones/Repository/ComisionRepository.java
@@ -1,13 +1,13 @@
 package com.pe.HeoComisiones.Repository;
 
-import com.pe.HeoComisiones.Entity.Comisiones;
-import com.pe.HeoComisiones.Entity.DetalleComisiones;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.pe.HeoComisiones.Entity.Comisiones;
 
 @Repository
 public interface ComisionRepository extends JpaRepository<Comisiones, Integer>{
